43a6796012495701d9b85b5e62715ba9b1a2e9d1
Robertson_Laptop <fjnrobe@yahoo.com>
2017-11-24 07:52:05 -0800

    initial commit

---
+++ src/com/bluelamp/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.bluelamp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.bluelamps")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+
+
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
